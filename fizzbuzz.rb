85a51b39495761b23b4a1567ae0d23892c505729
Matt Pawelski <mxpawels@gmail.com>
2021-06-07 02:24:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuz(number)
+  if number % 3 == 0 && number % 5 == 0
+    puts "Fizzbuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    return nil
+  end
+end
